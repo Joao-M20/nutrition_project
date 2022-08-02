f74fda23e890f5c00b2da2d5b8617124defa931f
Ivan João <78521275+Joao-M20@users.noreply.github.com>
2022-08-01 22:18:09 -0300

    .

--- nutri_lab/nutri_lab/settings.py
+++ nutri_lab/nutri_lab/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wk$82$md+*6!ssoyff3t+y@jqaycfwggu3pq3iy=_f9b#jevmz'
+SECRET_KEY = '{YOUR_SECRET_KEY}'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
